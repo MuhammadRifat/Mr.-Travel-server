20b9a4773904d64d4612e3899cba4b2eb6ab4510
Muhammad Rifat <hrifat450@gmail.com>
2021-04-18 00:42:09 +0600

    third day done

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ client.connect(err => {
     const toursCollection = client.db(`${process.env.DB_NAME}`).collection("tours");
     const adminCollection = client.db(`${process.env.DB_NAME}`).collection("admin");
     const bookingCollection = client.db(`${process.env.DB_NAME}`).collection("booking");
+    const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("reviews");
+
     console.log("database connected");
 
     app.get('/', (req, res) => {
@@ -32,49 +34,110 @@ client.connect(err => {
     app.post('/addTour', (req, res) => {
         const tourData = req.body;
         toursCollection.insertOne(tourData)
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     })
 
     app.post('/addAdmin', (req, res) => {
-        const email = req.body;
-        adminCollection.insertOne(email)
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
+        const adminData = req.body;
+        adminCollection.insertOne(adminData)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
     })
 
     app.post('/addBooking', (req, res) => {
         const bookingData = req.body;
         bookingCollection.insertOne(bookingData)
-        .then(result => {
-            res.send(result.insertedCount > 0);
-        })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.post('/bookings', (req, res) => {
+        const email = req.body.email;
+        const isAdmin = req.body.isAdmin;
+        let filter = { email: email };
+        if (isAdmin) {
+            filter = {};
+        }
+        bookingCollection.find(filter)
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addReview', (req, res) => {
+        const review = req.body;
+        reviewCollection.insertOne(review)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((err, documents) => {
+                res.send(documents.length > 0);
+            })
+    })
+
+    app.post('/toursByDestination', (req, res) => {
+        const location = req.body.location;
+        const type = req.body.type;
+        let filter = { location: new RegExp(location, 'i'), type: new RegExp(type, 'i') };
+        if(location === 'ALL'){
+            filter = { type: new RegExp(type, 'i') };
+        }
+        toursCollection.find(filter)
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     app.get('/tours', (req, res) => {
         toursCollection.find({})
-        .toArray( (err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/reviews', (req, res) => {
+        reviewCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     app.get('/tourById/:id', (req, res) => {
         const id = req.params.id;
-        toursCollection.find({_id: ObjectId(id)})
-        .toArray( (err, documents) => {
-            res.send(documents[0]);
-        })
+        toursCollection.find({ _id: ObjectId(id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
     })
 
     app.delete('/deleteTour/:id', (req, res) => {
-        toursCollection.deleteOne({_id: ObjectId(req.params.id)})
-        .then(result => {
-          res.send(result.deletedCount > 0);
-        })
-      })
-//   client.close();
+        toursCollection.deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+    app.patch('/updateBooking', (req, res) => {
+        const id = req.body.id;
+        const status = req.body.status;
+        bookingCollection.updateOne({ _id: ObjectId(id) },
+            {
+                $set: { status: status }
+            })
+            .then(result => {
+                res.send(result.modifiedCount > 0);
+            })
+    });
+    //   client.close();
 });
 
 app.listen(port);
